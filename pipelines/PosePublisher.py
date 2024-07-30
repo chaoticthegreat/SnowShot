46207223603a3495f2d840ca26256bc881e5131d
Ryan <ryan.ryanabraham@gmail.com>
2024-07-29 17:05:08 -0700

    :sparkles: added pose publisher

---
+++ pipelines/PosePublisher.py
@@ -0,0 +1,46 @@
+import math
+from typing import Union, List
+
+import ntcore
+
+from config.config import Configuration
+from pipelines.VisionDataClasses import Poses
+
+
+class PosePublisher():
+    _observations_pub: ntcore.DoubleArrayPublisher
+    _fps_pub: ntcore.IntegerPublisher
+
+    def initialize_publisher(self, config: Configuration):
+        nt_table = ntcore.NetworkTableInstance.getDefault().getTable(
+            '/' + config.local_config.device_id + '/output')
+        self._observations_pub = nt_table.getDoubleArrayTopic('poses').publish(
+            ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True))
+        self._fps_pub = nt_table.getIntegerTopic('fps').publish()
+
+    def send(self, timestamp: float, observation: Union[Poses, None],
+             fps: Union[int, None] = None) -> None:
+        # Initialize publishers on first call
+        # Send data
+        if fps is not None:
+            self._fps_pub.set(fps)
+        observation_data: List[float] = [0]
+        if observation is not None:
+            observation_data[0] = 1
+            observation_data.append(observation.pose1.translation().X())
+            observation_data.append(observation.pose1.translation().Y())
+            observation_data.append(observation.pose1.translation().Z())
+            observation_data.append(observation.pose1.rotation().getQuaternion().W())
+            observation_data.append(observation.pose1.rotation().getQuaternion().X())
+            observation_data.append(observation.pose1.rotation().getQuaternion().Y())
+            observation_data.append(observation.pose1.rotation().getQuaternion().Z())
+            if observation.pose2 is not None:
+                observation_data[0] = 2
+                observation_data.append(observation.pose2.translation().X())
+                observation_data.append(observation.pose2.translation().Y())
+                observation_data.append(observation.pose2.translation().Z())
+                observation_data.append(observation.pose2.rotation().getQuaternion().W())
+                observation_data.append(observation.pose2.rotation().getQuaternion().X())
+                observation_data.append(observation.pose2.rotation().getQuaternion().Y())
+                observation_data.append(observation.pose2.rotation().getQuaternion().Z())
+        self._observations_pub.set(observation_data, math.floor(timestamp * 1000000))
--- snowshot.py
+++ snowshot.py
@@ -1,15 +1,69 @@
-from config.ConfigManager import load_calibration_config, load_nt_config, ConfigManager
+import dataclasses
+import time
+
+import cv2
+import ntcore
+import numpy as np
+
+from config.ConfigManager import load_calibration_config, load_nt_config, ConfigManager, config_changed
 from config.config import Configuration, NetworkTablesConfig, CalibrationConfig, CameraConfig
+from pipelines.ArucoPipeline import ArucoPipeline
+from pipelines.CameraPoseEstimator import solve_camera_pose
+from pipelines.Pipeline import Pipeline
+from pipelines.PosePublisher import PosePublisher
 
 CALIBRATION_FILENAME = "calibration.json"
 CONFIG_FILENAME = "ntconfig.json"
+
+
 def main():
     config_data = Configuration(NetworkTablesConfig(), CalibrationConfig(), CameraConfig())
-
+    last_config = Configuration(NetworkTablesConfig(), CalibrationConfig(), CameraConfig())
     config_manager = ConfigManager()
 
     load_calibration_config(config_data, CALIBRATION_FILENAME)
+    if config_data.calibConfig.cameraMatrix == np.array(
+            []) or config_data.calibConfig.distortionCoefficients == np.array([]):
+        raise Exception("Calibration not loaded!")
     load_nt_config(config_data, CONFIG_FILENAME)
+
+    ntcore.NetworkTableInstance.getDefault().setServer(config_data.ntConfig.serverIP)
+    ntcore.NetworkTableInstance.getDefault().startClient4(config_data.ntConfig.deviceID)
     config_manager.initialize_subscribers(config_data)
+
+    pipeline: Pipeline = ArucoPipeline(cv2.aruco.DICT_APRILTAG_36h11)
+    pipeline.initialize(config_data)
+    pipeline.initialize_publisher(config_data)
+
+    pose_publisher = PosePublisher()
+    pose_publisher.initialize_publisher(config_data)
+
+    frames = 0
+    last_print = 0
     while True:
+        last_config = Configuration(dataclasses.replace(config_data.ntConfig), dataclasses.replace(config_data.calibConfig), dataclasses.replace(config_data.cameraConfig))
         config_manager.update_camera_config(config_data)
+        fps = None
+        frames += 1
+        if time.time() - last_print > 1:
+            last_print = time.time()
+            fps = frames
+            print('Running at', frames, 'fps')
+            frame_count = 0
+        if config_changed(last_config, config_data):
+            pipeline.reset_config(config_data)
+
+        success, frame = pipeline.get_frame()
+        if not success:
+            pipeline.reset_config(config_data)
+            continue
+        tags = pipeline.process_frame(frame)
+        poses = solve_camera_pose(tags, config_data.cameraConfig.tagLayout)
+
+        pipeline.send_to_nt(tags, config_data, time.time())
+        pose_publisher.send(time.time(), poses, fps)
+
+
+
+if __name__ == "__main__":
+    main()
